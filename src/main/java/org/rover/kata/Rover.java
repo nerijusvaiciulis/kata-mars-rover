9bc2d9e7526669c25006c4cb1eae6daecd6f98b8
Nerijus Vaičiulis <aikidistas@gmail.com>
2022-02-02 15:33:58 +0200

    rover receive one command forward or backward when initialized in any directions

--- src/main/java/org/rover/kata/Rover.java
+++ src/main/java/org/rover/kata/Rover.java
@@ -1,8 +1,48 @@
 package org.rover.kata;
 
-public record Rover (int x, int y, Direction direction) {
+import static org.rover.kata.Direction.*;
+
+public final class Rover {
+    private int x;
+    private int y;
+    private final Direction direction;
+
+    public Rover(int x, int y, Direction direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
 
 
     public void receiveCommand(String command) {
+        if (EAST == direction && "F".equals(command)) {
+            x += 1;
+        } else if (NORTH == direction && "F".equals(command)) {
+            y += 1;
+        } else if (EAST == direction && "B".equals(command)) {
+            x -= 1;
+        } else if (NORTH == direction && "B".equals(command)) {
+            y -= 1;
+        } else if (WEST == direction && "F".equals(command)) {
+            x-= 1;
+        } else if (WEST == direction && "B".equals(command)) {
+            x+= 1;
+        } else if (SOUTH == direction && "F".equals(command)) {
+            y-= 1;
+        } else if (SOUTH == direction && "B".equals(command)) {
+            y+= 1;
+        }
+    }
+
+    public int x() {
+        return x;
+    }
+
+    public int y() {
+        return y;
+    }
+
+    public Direction direction() {
+        return direction;
     }
 }
--- src/test/java/org/rover/kata/RoverTest.java
+++ src/test/java/org/rover/kata/RoverTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.rover.kata.Direction.*;
 
 public class RoverTest {
 
@@ -31,5 +32,71 @@ public class RoverTest {
     @Test
     public void whenReceiveCommandThenExecuteAction() {
         rover.receiveCommand("");
+    }
+
+    @Test
+    public void givenDirectionEastWhenReceiveCommandFThenXIncrease() {
+        rover.receiveCommand("F");
+        assertThat(rover.x()).isEqualTo(x + 1);
+    }
+
+    @Test
+    public void givenDirectionEastWhenReceiveCommandBThenXDecrease() {
+        rover.receiveCommand("B");
+        assertThat(rover.x()).isEqualTo(x - 1);
+    }
+
+    @Test
+    public void givenDirectionNorthWhenReceiveCommandFThenYIncrease() {
+        rover = new Rover(x, y, NORTH);
+
+        rover.receiveCommand("F");
+
+        assertThat(rover.y()).isEqualTo(y + 1);
+    }
+
+    @Test
+    public void givenDirectionNorthWhenReceiveCommandBThenYDecrease() {
+        rover = new Rover(x, y, NORTH);
+
+        rover.receiveCommand("B");
+
+        assertThat(rover.y()).isEqualTo(y - 1);
+    }
+
+    @Test
+    public void givenDirectionWestWhenReceiveCommandFThenXDecrease() {
+        rover = new Rover(x, y, WEST);
+
+        rover.receiveCommand("F");
+
+        assertThat(rover.x()).isEqualTo(x - 1);
+    }
+
+    @Test
+    public void givenDirectionWestWhenReceiveCommandBThenXIncrease() {
+        rover = new Rover(x, y, WEST);
+
+        rover.receiveCommand("B");
+
+        assertThat(rover.x()).isEqualTo(x + 1);
+    }
+
+    @Test
+    public void givenDirectionSouthWhenReceiveCommandFThenYDecrease() {
+        rover = new Rover(x, y, SOUTH);
+
+        rover.receiveCommand("F");
+
+        assertThat(rover.y()).isEqualTo(y - 1);
+    }
+
+    @Test
+    public void givenDirectionSouthWhenReceiveCommandBThenYIncrease() {
+        rover = new Rover(x, y, SOUTH);
+
+        rover.receiveCommand("B");
+
+        assertThat(rover.y()).isEqualTo(y + 1);
     }
 }
