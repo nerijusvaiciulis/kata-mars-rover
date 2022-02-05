e35b8f0826100f3a5d44d299289050335aafab96
Nerijus Vaičiulis <aikidistas@gmail.com>
2022-02-05 18:22:15 +0200

    implement S..... S....... feedback
    both x and y are basically constants. Instead of naming the variable "some" x, rename it to something meaningful, maybe START_POS_X or whatever

--- src/test/java/org/rover/kata/RoverTest.java
+++ src/test/java/org/rover/kata/RoverTest.java
@@ -6,17 +6,17 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RoverTest {
-    private final Direction direction = Direction.EAST;
-    private final int x = 4;
-    private final int y = 2;
+    private final Direction START_DIRECTION = Direction.EAST;
+    private final int START_COORDINATE_X = 4;
+    private final int START_COORDINATE_Y = 2;
     private Location location;
     private Location expectedLocation;
     private Rover rover;
 
     @BeforeEach
     void setUp() {
-        location = new Location(x, y, direction);
-        expectedLocation = new Location(x, y, direction);
+        location = new Location(START_COORDINATE_X, START_COORDINATE_Y, START_DIRECTION);
+        expectedLocation = new Location(START_COORDINATE_X, START_COORDINATE_Y, START_DIRECTION);
 
         rover = new Rover(location);
     }
@@ -24,18 +24,18 @@ public class RoverTest {
     @Test
     public void
     whenRoverIsInitializedThenCoordinatesAreSet() {
-        assertThat(rover.x()).isEqualTo(x);
-        assertThat(rover.y()).isEqualTo(y);
+        assertThat(rover.x()).isEqualTo(START_COORDINATE_X);
+        assertThat(rover.y()).isEqualTo(START_COORDINATE_Y);
     }
 
     @Test
     public void whenRoverIsInitializedThenDirectionIsSet() {
-        assertThat(rover.direction()).isEqualTo(direction);
+        assertThat(rover.direction()).isEqualTo(START_DIRECTION);
     }
 
     @Test
     public void whenRoverIsInitializedThenLocationIsSet() {
-        var rover = new Rover(x, y, direction);
+        var rover = new Rover(START_COORDINATE_X, START_COORDINATE_Y, START_DIRECTION);
 
         assertThat(rover.location()).isEqualTo(expectedLocation);
     }
