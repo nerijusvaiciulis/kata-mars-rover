a3b580b087f650fe6217d6515ab8ec794925395a
Nerijus Vaičiulis <aikidistas@gmail.com>
2022-02-05 17:43:34 +0200

    implement S..... S....... feedback
    value value in enum has a specific name, ordinal. Direction.<NORTH/EAST/SOUTH/WEST>.ordinal() will give the same value. It's dangerous in a sense, that enum can be altered and ordinals would change. But in case of "directions", it's a safe bet, no one will invent additional direction, as there is none, these are constant values. The risk is if someone would decide to alter enum and add NORTHEAST. Just keep this in mind.

--- src/main/java/org/rover/kata/Direction.java
+++ src/main/java/org/rover/kata/Direction.java
@@ -1,24 +1,18 @@
 package org.rover.kata;
 
 public enum Direction {
-    NORTH(0),
-    EAST(1),
-    SOUTH(2),
-    WEST(3);
-
-    int value;
-
-    Direction(int i) {
-        this.value = i;
-    }
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST;
 
     public Direction left() {
-        int index = (value + 3) % 4;
+        int index = (ordinal() + 3) % 4;
         return Direction.values()[index];
     }
 
     public Direction right(){
-        int index = (value + 1) % 4;
+        int index = (ordinal() + 1) % 4;
         return Direction.values()[index];
     }
 }
